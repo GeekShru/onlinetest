c2c6ba140975e5e05490523d38948580db291efe
GeekShru <44857788+GeekShru@users.noreply.github.com>
2018-11-16 08:27:27 +0530

    Create codetest1.py
    Ouput of above program:
    =============================
    C:\Python27\Project\Scripts\python.exe C:/Users/mns1/PycharmProjects/Project1/codetest.py
    
    1 This is an outline
     
     + It's not a very good outline
        
      - I've seen better
         
      + I've seen worse
         
       - I saw a really bad one back in 2008
          
    2 This is the second numbered item in the outline
          
     - This is sub text that spans multiple lines
        
       This should be included with the previous line
        
       And this line too
        
      - That is more sub text
         
    3 Numbers
         
     - Some text
        
    3.1 Lots
        
     - Some more text
        
     - And some more
        
    3.1.1 And lots
        
     + One
       
      + Two
         
       - Three
          
    3.1.1.1 Of numbers
          
     - Another line
        
    3.2 Less Numbers
        
     - Text
        
    3.1.1 More Numbers
        
     - Blah
        
    4 One number again
     
     - The last line
    
    Process finished with exit code 0

---
+++ codetest1.py
@@ -0,0 +1,65 @@
+def check_indent(s1, cur, prev, laststr):
+    ind = str(cur)
+    prev = len(s1)
+    ls1 = len(laststr)
+    #print cur, prev, ls1
+    cnt = i = 1
+    while(cnt<prev):
+        if(ls1>prev):
+            i += 1
+        ind += '.'+str(i)
+        cnt += 1
+    return ind
+
+def read_lines_from_file(filename):
+    data = open(filename, 'r')
+    lst = data.readlines()
+    return lst
+
+def format_text(lst):
+    n = len(lst)
+    cur = prev = 0
+    laststr = 0
+    c = l = 0
+    for line in lst:
+       c += 1
+       if line == '\n':
+            continue
+       else:
+        tmp = line.split(' ')
+        checkstr = tmp[0]
+        if checkstr.startswith('*'):
+            if (len(checkstr) == 1):
+                cur += 1
+                print str(cur) +' '+ ' '.join(tmp[1:])
+            else:
+                prev = cur
+                ind = check_indent(checkstr, cur, prev, laststr)
+                print ind +' '+' '.join(tmp[1:])
+            laststr = checkstr
+
+        elif checkstr.startswith("."):
+            cl = 0
+            text1 = ""
+            l = len(checkstr)
+            if(c < n-1):
+                nchs = lst[c+1].split(' ')[0]
+                if(nchs.startswith('.')):
+                    cl = len(nchs)
+                    #print nchs
+            #print cl,l
+            if(l<cl):
+                text1 += '+'
+            else:
+                text1 += '-'
+
+            text1 += ' '+' '.join(tmp[1:])
+            print "".ljust(l)+text1
+        else:
+            line = '  '+line.strip('\n')
+            print "".ljust(l)+line
+
+if __name__ == '__main__':
+    file = "C:\Users\mns1\Documents\codetest.txt"
+    res = read_lines_from_file(file)
+    format_text(res)
